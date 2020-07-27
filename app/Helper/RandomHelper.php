106a2817f1701cf7c861bae459d0a654b6b6341c
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-07-27 21:51:51 +0430

    helper class support generating randome numbers for the Lottery class code attribute.

---
+++ app/Helper/RandomHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Helper;
+
+
+class RandomHelper 
+{
+    /**
+     * Generate a more truly "random" alpha-numeric string.
+     *
+     * @param  int  $length
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    public static function random($length = 16)
+    {
+        if ( ! function_exists('openssl_random_pseudo_bytes'))
+        {
+            throw new RuntimeException('OpenSSL extension is required.');
+        }
+
+        $bytes = openssl_random_pseudo_bytes($length * 2);
+
+        if ($bytes === false)
+        {
+            throw new RuntimeException('Unable to generate random string.');
+        }
+
+        return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $length);
+    }
+
+    /**
+     * Generate a "random" alpha-numeric string.
+     *
+     * Should not be considered sufficient for cryptography, etc.
+     *
+     * @param  int  $length
+     * @return string
+     */
+    public static function quickRandom($length = 10)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $salt = str_random(10);
+        return $salt . substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    }
+}
+
+
+
